446cdedc98b166cce21ff8e447c8c2b670da8a03
owendong527 <owendong900995@gmail.com>
2024-10-15 14:07:52 +0800

    modify x-axis

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,61 +1,60 @@
 <template>
-  <NavbarComponent></NavbarComponent>
-  <RouterView></RouterView>
+  <div class="overflow-hidden">
+    <NavbarComponent></NavbarComponent>
+    <RouterView></RouterView>
 
-  <footer class="pt-2 footer-bg footer-text">
-    <ul class="list-unstyled d-lg-flex justify-content-lg-around">
-      <li class="p-3 text-center">
-        <RouterLink to="/"><img style="width: 75%;" class="logo object-fit"
-          src="@/assets/images/logo/logo9.png" alt="logo">
-        </RouterLink>
-      </li>
-      <li class="p-3">
-        <p class="h3 text-white">聯絡我們</p>
-        <p class=" p-2 text-white">
-          <a class="nav-link" href="tel:(02) 1234-5678">
-          (02) 1234-5678
-          </a>
-          <a class="nav-link" href="mailto:owendong900995@gmail.com">
-            owendong900995@gmail.com
-          </a>
-          MON - SUN : 8:30 am - 17:30 pm
-        </p>
-        <ul class="list-unstyled d-flex">
-          <li>
-            <a class="pe-3 nav-link" href="#"><i class="h2 bi bi-facebook"></i></a>
-          </li>
-          <li>
-            <a class="pe-3 nav-link" href="#"><i class="h2 bi bi-instagram"></i></a>
-          </li>
-          <li>
-            <a class="pe-3 nav-link" href="#"><i class="h2 bi bi-envelope"></i></a>
-          </li>
-        </ul>
-        <RouterLink class="nav-item nav-link my-4" to="/login">
-            老師登入</RouterLink>
-      </li>
-    </ul>
-    <div class="text-center mt-1">
-      <p class="small text-secondary m-0">本網站僅供作品參考，並非商品真實營運販售，若有侵權請來信告知，將立即處理</p>
-      <p class="small text-secondary m-0">
-        Copyright ©Liaoyu All Rights Reserved. Designed by Owen</p>
+    <footer class="pt-2 footer-bg footer-text">
+      <ul class="list-unstyled d-lg-flex justify-content-lg-around">
+        <li class="p-3 text-center">
+          <RouterLink to="/"><img style="width: 75%;" class="logo object-fit"
+            src="@/assets/images/logo/logo9.png" alt="logo">
+          </RouterLink>
+        </li>
+        <li class="p-3">
+          <p class="h3 text-white">聯絡我們</p>
+          <p class=" p-2 text-white">
+            <a class="nav-link" href="tel:(02) 1234-5678">
+            (02) 1234-5678
+            </a>
+            <a class="nav-link" href="mailto:owendong900995@gmail.com">
+              owendong900995@gmail.com
+            </a>
+            MON - SUN : 8:30 am - 17:30 pm
+          </p>
+          <ul class="list-unstyled d-flex">
+            <li>
+              <a class="pe-3 nav-link" href="#"><i class="h2 bi bi-facebook"></i></a>
+            </li>
+            <li>
+              <a class="pe-3 nav-link" href="#"><i class="h2 bi bi-instagram"></i></a>
+            </li>
+            <li>
+              <a class="pe-3 nav-link" href="#"><i class="h2 bi bi-envelope"></i></a>
+            </li>
+          </ul>
+          <RouterLink class="nav-item nav-link my-4" to="/login">
+              老師登入</RouterLink>
+        </li>
+      </ul>
+      <div class="text-center mt-1">
+        <p class="small text-secondary m-0">本網站僅供作品參考，並非商品真實營運販售，若有侵權請來信告知，將立即處理</p>
+        <p class="small text-secondary m-0">
+          Copyright ©Liaoyu All Rights Reserved. Designed by Owen</p>
+      </div>
+    </footer>
+
+    <div>
+      <a
+        href="#"
+        class="btn btn-dark toTop justify-content-center align-items-center"
+        :class="{show:isShow}"
+        @click.prevent="scrollToTop"
+      >
+        <span class="icon">
+          <i class="bi bi-arrow-up-circle-fill"></i>
+        </span>
+      </a>
     </div>
-  </footer>
-
-  <!-- <button type="button" class="scroll-to-top-btn" @click="scrollToTop">
-    <i class="bi bi-arrow-bar-up"></i>
-  </button> -->
-  <div>
-    <a
-      href="#"
-      class="btn btn-dark toTop justify-content-center align-items-center"
-      :class="{show:isShow}"
-      @click.prevent="scrollToTop"
-    >
-      <span class="icon">
-        <i class="bi bi-arrow-up-circle-fill"></i>
-      </span>
-    </a>
   </div>
 </template>
 
@@ -100,5 +99,8 @@ export default {
 <style scoped>
   .footer-bg {
     background-color: #523e27;
+  }
+  .overflow-hidden {
+    overflow: hidden;
   }
 </style>
